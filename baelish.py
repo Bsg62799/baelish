cea7d7263b80da19d52bd0bed44fd6c933313138
Bryce Griffin <griffin.b@husky.neu.edu>
2020-05-11 15:05:06 -0400

    Finishing initial setup questions

--- baelish.py
+++ baelish.py
@@ -4,85 +4,168 @@
 # imports
 from pyfiglet import Figlet
 import clint
-import PyInquirer
+from PyInquirer import Validator, ValidationError
+from PyInquirer import prompt, style_from_dict, Token
 import os
 import subprocess
 
-# global vars
-project = ""
+# Project class, which holds all necessary fields for the current project
+class Project:
+    def __init__(self, target, directory, description):
+        self.target = target
+        self.dir = directory
+        self.desc = description
+        self.ports = []
+        self.loot = []
+
+# Dictionary of available functions
+""" commands = {
+    'scan': scan,
+
+
+} """
+
+
+
+# Check that the given host is legitimate and reachable
+class HostValidator(Validator):
+    def validate(self, document):
+        try:
+            subprocess.check_output("ping -c 1 " + document.text, shell=True)
+        except:
+            raise ValidationError(
+                message='Given target is invalid or unreachable. Try again:',
+                cursor_position=len(document.text))
+
+
+# Check that the given project directory does not exist and can be created
+class NewDirValidator(Validator):
+    def validate(self, document):
+
+        # Set effective path
+        effective_path = os.getcwd() + "/" + document.text
+
+        if os.path.isdir(effective_path):
+            raise ValidationError(
+            message='Directory already exists at given path. Try again',
+            cursor_position=len(document.text))
+
+
+        else:
+
+            try:
+                os.mkdir(effective_path)
+                os.mkdir(effective_path + "/exploit")
+                os.mkdir(effective_path + "/loot")
+                os.mkdir(effective_path + "/notes")
+                os.mkdir(effective_path + "/enumeration")
+
+                global project
+                project = effective_path
+
+            except:
+                raise ValidationError(
+                message='Could not create project directories at given path. Try again',
+                cursor_position=len(document.text))
+
+# Check that the given project directory exists and has the necessary subdirectories
+class OldDirValidator(Validator):
+    def validate(self, document):
+
+        # Set effective path
+        effective_path = os.getcwd() + "/" + document.text
+
+        if not os.path.isdir(effective_path):
+            raise ValidationError(
+            message='Directory does not exist at given path. Try again',
+            cursor_position=len(document.text))
+
+
+        else:
+
+            if os.path.isdir(effective_path + "/exploit") and os.path.isdir(effective_path + "/loot") and os.path.isdir(effective_path + "/notes") and os.path.isdir(effective_path + "/enumeration"):
+                global project
+                project = effective_path
+
+            else:
+                raise ValidationError(
+                message='Missing subdirectories at given path, not a baelish project. Try again',
+                cursor_position=len(document.text))
+
+
+
+
+
+# List of necessary setup questions
+questions = [
+
+    {
+        'type': 'confirm',
+        'name': 'new',
+        'message': 'Create a new project?',
+        'default': True
+    },
+
+    {
+        'type': 'input',
+        'name': 'newDir',
+        'message': 'Relative path for new project?',
+        'validate': NewDirValidator,
+        'when': lambda info: info['new'] == True
+    },
+
+    {
+        'type': 'input',
+        'name': 'oldDir',
+        'message': 'Relative path for exisiting project?',
+        'validate': OldDirValidator,
+        'when': lambda info: info['new'] == False
+
+    },
+
+    {
+        'type': 'input',
+        'name': 'target',
+        'message': 'Target IP address?',
+        'validate': HostValidator
+    },
+
+    {
+        'type': 'input',
+        'name': 'desc',
+        'message': 'Description of this project?'
+    }
+]
 
 # Run a scan of all ports for target host
-def enumerate(host):
+def scan(host):
 
     # Verify that the host is reachable
 
-    # output = subprocess.check_output("ping -c 1 " + host, shell=True)
+    output = subprocess.check_output("ping -c 1 " + host, shell=True)
 
-    # if not b'64 bytes from' in output:
+    if not b'64 bytes from' in output:
 
-        # raise Exception("Given host is not reachable")
+        raise Exception("Given host is not reachable")
 
     # Scan for ports, services and OS
     print("Scanning...")
-    print(project)
     os.system('nmap -A -p- -o ' + project + '/enumeration/nmap.txt ' + host + ' > /dev/null')
 
-
-# Generate a report based on notes stored in project directory
-# def report():
-
-
-
-def initialize(effective_path):
-
-    # Create several sub directories for the project
-    os.mkdir(effective_path + "/exploit")
-    os.mkdir(effective_path + "/loot")
-    os.mkdir(effective_path + "/notes")
-    os.mkdir(effective_path + "/enumeration")
-
-
-
-# Check to see if the given relative path exists
-#   - if so, set that folder as the working directory
-#   - if not, create a directory at the given relative path
-def project_directory(path):
-
-    # Combined cwd and given path
-    effective_path = os.getcwd() + "/" + path
-
-    print("Effective path: " + effective_path)
-
-    # Verify if the project exists
-    if os.path.isdir(effective_path):
-
-        print("found")
-
-    else:
-        try:
-
-            # Otherwise create a new project directory
-            os.mkdir(effective_path)
-            initialize(effective_path)
-
-        except:
-            raise Exception("Error: Could not write to path")
-
-
-    global project
-    project = effective_path
 
 def main():
 
     # Print Title
     format = Figlet(font='slant')
-    print(clint.textui.colored.red(format.renderText('Cyber Scribe')))
+    print(clint.textui.colored.yellow('------------------------------------'))
+    print(clint.textui.colored.yellow(format.renderText('Baelish')))
+    print(clint.textui.colored.yellow('       \x1B[3m\"Knowledge is power\"\x1B[23m'))
+    print(clint.textui.colored.yellow('------------------------------------\n'))
 
-    # Print Greeting
-    print('Welcome to Cyber Scribe')
+    # Get project information
+    info = prompt(questions, question_styles)
 
-    project_directory("Tester")
-    enumerate("10.10.10.171")
+    # Create
 
 if __name__ == '__main__':
     main()
